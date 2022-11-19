25f9c5f606be8ea72f9dd71070d0a3edf3f05711
deviant <mb.deviant@gmail.com>
2022-11-19 07:28:23 +0300

    create primitive visual and functions

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const container = document.getElementById('card-container');
+const newBookButton = document.getElementById('new-book');
+
+
 let myLibrary = [];
 let log = console.log;
 
-function Book(title,author,pages){
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,16 +13,44 @@ function Book(title,author,pages){
 
 }
 
-function addToLibrary(book){
+function addToLibrary(book) {
     myLibrary.push(book);
 }
 
-addToLibrary.prototype = Object.create(Book.prototype);
+newBookButton.addEventListener('click',()=>{
+    
+});
 
-let newbook = new Book('ahmet','mehmet','12');
-let anotherBook = new Book('foo','bar','152')
+let newbook = new Book('ahmet', 'mehmet', '12');
+let anotherBook = new Book('foo', 'bar', '152');
+let yetAnotherBook = new Book('bar', 'foo', '512');
+let aNewOne = new Book('alice', 'potato', '231');
 addToLibrary(newbook);
 addToLibrary(anotherBook);
+addToLibrary(yetAnotherBook);
+addToLibrary(aNewOne);
 
-log(myLibrary[1].pages);
+function createBookCard(){
+    myLibrary.forEach((item)=>{
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+
+        title.innerHTML = item.title;
+        author.innerHTML = item.author;
+        pages.innerHTML = item.pages;
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+
+        container.appendChild(bookCard);
+    });
+}
+createBookCard();
+
+
 
