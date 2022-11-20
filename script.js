b6c5947b197a5bfb79bbd6b3354f8cc894fdb5ed
deviant <mb.deviant@gmail.com>
2022-11-21 01:58:08 +0300

    optimize main functions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.getElementById('card-container');
 const newBookButton = document.getElementById('new-book');
 const overlay = document.getElementById('form-overlay');
-const form = document.getElementById('form-container');
-const submitButton = document.getElementById('book-submit');
+const form = document.getElementById('book-form');
+
 
 
 let myLibrary = [];
@@ -16,57 +16,59 @@ function Book(title, author, pages) {
 
 }
 
-function addToLibrary(book) {
-    myLibrary.push(book);
-}
-
 newBookButton.addEventListener('click', () => {
     overlay.style.display = 'block';
 });
 
 overlay.addEventListener('click', (e) => {
-    if (e.target == overlay) overlay.style.display = 'none';
-})
-
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    addNewBook();
+    if (e.target == overlay) closeOverlay();
 });
 
 
-function createBookCard() {
-
-    myLibrary.forEach((item) => {
-
-        const bookCard = document.createElement('div');
-        bookCard.classList.add('book-card');
-        const title = document.createElement('p');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-
-        title.innerHTML = item.title;
-        author.innerHTML = item.author;
-        pages.innerHTML = item.pages;
-
-        bookCard.appendChild(title);
-        bookCard.appendChild(author);
-        bookCard.appendChild(pages);
-        container.appendChild(bookCard);
-        
-    });
-}
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    addToLibrary();
+    createBookCard();
+    closeOverlay()
+    log(myLibrary);
+})
 
 
-function addNewBook() {
+
+function addToLibrary() {
     const bookTitle = document.getElementById('book-title').value;
     const bookAuthor = document.getElementById('book-author').value;
     const bookPages = document.getElementById('book-pages').value;
-
-
     let newBook = new Book(bookTitle, bookAuthor, bookPages);
-    addToLibrary(newBook);
-    
-    createBookCard();
 
-} 
+    myLibrary.push(newBook);
+    form.reset();
+}
+
+function updateDisplay() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        createBookCard(myLibrary[i]);
+    }
+}
+
+function createBookCard() {
+    const bookCard = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    bookCard.classList.add('book-card');
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        title.innerHTML = myLibrary[i].title;
+        author.innerHTML = myLibrary[i].author;
+        pages.innerHTML = myLibrary[i].pages;
+    }
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    container.appendChild(bookCard);
+}
+function closeOverlay() {
+    overlay.style.display = 'none';
+}
