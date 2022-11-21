ac6c4f5c38b157c7769451ba9ea6697f32bcc36f
deviant <mb.deviant@gmail.com>
2022-11-21 05:23:28 +0300

    add function to book cards

--- script.js
+++ script.js
@@ -24,15 +24,13 @@ overlay.addEventListener('click', (e) => {
     if (e.target == overlay) closeOverlay();
 });
 
-
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     addToLibrary();
     createBookCard();
-    closeOverlay()
+    closeOverlay();
     log(myLibrary);
 })
-
 
 
 function addToLibrary() {
@@ -41,34 +39,47 @@ function addToLibrary() {
     const bookPages = document.getElementById('book-pages').value;
     let newBook = new Book(bookTitle, bookAuthor, bookPages);
 
+
     myLibrary.push(newBook);
     form.reset();
 }
 
-function updateDisplay() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        createBookCard(myLibrary[i]);
-    }
-}
-
-function createBookCard() {
+function createBookCard(i) {
     const bookCard = document.createElement('div');
     const title = document.createElement('p');
     const author = document.createElement('p');
     const pages = document.createElement('p');
+    const removeButton = document.createElement('button');
+    const readStatus = document.createElement('button');
     bookCard.classList.add('book-card');
 
+    removeButton.innerHTML = 'Remove';//not functional
+    readStatus.innerHTML = 'Read Status';
+
     for (let i = 0; i < myLibrary.length; i++) {
-        title.innerHTML = myLibrary[i].title;
-        author.innerHTML = myLibrary[i].author;
-        pages.innerHTML = myLibrary[i].pages;
+        title.innerHTML = `"${myLibrary[i].title}"`;
+        author.innerHTML = `by ${myLibrary[i].author}`;
+        pages.innerHTML = `${myLibrary[i].pages} pages`;
     }
+
+    readStatus.addEventListener('click', () => {
+        (readStatus.innerHTML == 'Read') ? readStatus.innerHTML = 'Not Read' : readStatus.innerHTML = 'Read';
+        (readStatus.innerHTML == 'Read') ? readStatus.style.background = 'Green' : readStatus.style.background = 'Red';
+    });
+    removeButton.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(i), 1);
+        container.removeChild(bookCard);
+        log(myLibrary);
+    });
 
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
+    bookCard.appendChild(removeButton);
+    bookCard.appendChild(readStatus);
     container.appendChild(bookCard);
 }
+
 function closeOverlay() {
     overlay.style.display = 'none';
 }
